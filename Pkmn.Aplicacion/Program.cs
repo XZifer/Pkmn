6d5d51234922c709684629b06ab4501cefdb537f
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-21 14:09:02 -0500

    Agregar archivos de proyecto.

---
+++ Pkmn.Aplicacion/Program.cs
@@ -0,0 +1,30 @@
+﻿using Pkmn.Datos;
+using Pkmn.Logica.Clases;
+using Pkmn.Modelos.Clases;
+using Pkmn.Modelos.Enumeradores;
+
+Pokedex pokedex = new Pokedex();
+
+string ficha = pokedex.ObtenerFicha("Bulbasaur");
+
+//Console.WriteLine(ficha);
+
+Pokemones pokemones = new Pokemones();
+Pokemon[] pokemonArray = pokemones.ObtenerPkmns();
+
+string ficha2 = pokedex.IdentificarPkmn(pokemonArray[0]);
+
+//Console.WriteLine(ficha2);
+
+Pokebola pokebolaRoja = new Pokebola();
+Console.WriteLine("Tiene un pokemon la pokebolaroja:"+pokebolaRoja.EsOcupada());
+
+pokebolaRoja.pokemonDentroPokebola = pokemonArray[1];
+Console.WriteLine("Tiene un pokemon la pokebolaroja:" + pokebolaRoja.EsOcupada());
+
+string fichaPokemonEnPokebola = pokedex.IdentificarPkmn(pokebolaRoja.pokemonDentroPokebola);
+Console.WriteLine(fichaPokemonEnPokebola);
+
+pokebolaRoja.pokemonDentroPokebola = null;
+Console.WriteLine("Tiene un pokemon la pokebolaroja:" + pokebolaRoja.EsOcupada());
+
---
+++ Pkmn.Logica/Clases/Pokebola.cs
@@ -0,0 +1,26 @@
+﻿using Pkmn.Modelos.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pkmn.Logica.Clases
+{
+    public class Pokebola
+    {
+        public Pokemon? pokemonDentroPokebola { get; set; }
+        public bool EsOcupada()
+        {
+            if (pokemonDentroPokebola == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            //return pokemonDentroPokebola != null;   
+        }
+    }
+}
